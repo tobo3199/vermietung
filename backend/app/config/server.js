57a0b83b1174496999fe51a2b8e4dcffce12d9ac
tobo3199 <tobias.sauter.003@outlook.com>
2022-01-16 13:42:00 +0100

    backend deleted

--- backend/app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '5dacb0a85f99d8a7f5874e45a9a5d08a'),
-    },
-  },
-});
